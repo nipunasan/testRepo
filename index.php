112bb7f8bf334f1144c0177017ef3e1f2ddce64b
Nipuna S Perera <35953082+nipunasan@users.noreply.github.com>
2022-07-17 14:17:47 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$env="env_ZW52";
+$dbUrl="dbUrl_ZGJVcmw=";
+$dbUsername="dbUsername_ZGJVc2VybmFtZQ==";
+$dbPassword="dbPassword_ZGJQYXNzd29yZA==";
+
+echo "<html>";
+echo "<body>";
+echo "<H1>APP Environment: ".$env."</H1>";
+echo "</br>";
+echo "<h2>DB URL: ".$dbUrl."</h2></br>";
+echo "<h2>DB Username: ".$dbUsername."</h2></br>";
+echo "<h2>DB Password: ".$dbPassword."</h2></br>";
+echo "</body>";
+echo "</html>";
+?>
